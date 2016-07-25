1aa982c48fca2b019f1163df000fdef93347f118
chenyingsho <601668970@qq.com>
2016-07-25 18:34:15 +0800

    create CartItems class

---
+++ src/models/cartItems.js
@@ -0,0 +1,11 @@
+class CartItems {
+
+  constructor(barcode, name, unit, price = 0) {
+    this.barcode = barcode;
+    this.name = name;
+    this.unit = unit;
+    this.price = price;
+  }
+}
+
+module.exports = CartItems;
