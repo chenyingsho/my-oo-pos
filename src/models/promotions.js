3606deebc8e74856bc1641976e4851bce1bbead2
chenyingsho <601668970@qq.com>
2016-07-25 20:15:09 +0800

    replace promotion object in fixtures

---
+++ src/models/promotions.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chenying on 16-7-25.
+ */
+class Promotion {
+
+  constructor(type, barcodes = []) {
+    this.type = type;
+    this.barcodes = barcodes;
+  }
+}
+
+module.exports = Promotion;
